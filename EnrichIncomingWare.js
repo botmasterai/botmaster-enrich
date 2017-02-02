3b18e56b058aa623d0caf812efa30c1beb1fc8c0
Jonathan R Wickens <wickensj@uk.ibm.com>
2017-02-02 15:07:03 +0100

    add required module from last commit

---
+++ EnrichIncomingWare.js
@@ -0,0 +1,41 @@
+const R = require('ramda');
+const debug = require('debug')('botmaster:incoming:enrich');
+const util = require('util');
+const {Enrich} = require('./Enrich');
+
+/**
+ * Factory function to generate incoming ware for enrich
+ * @param  {Object} enrichers an object of enrichers
+ * @param  {String} [sessionPath] dot denoted path find the context in the update. defaults to 'context'
+ * @param  {Object} [params] optional additional params to pass to enrichers
+ * @return {Function} botmaster middleware
+ */
+const EnrichIncomingWare = ({enrichers, sessionPath = 'context', params = {}}) =>
+    (bot, update, next) => {
+        params.bot = bot;
+        params.update = update;
+        const enrich = Enrich(params);
+        sessionPath = sessionPath.split('.');
+        const lensContext = R.lensPath(sessionPath);
+        const oldContext = R.compose(
+            R.defaultTo({}),
+            R.view(lensContext)
+        )(update);
+        debug(`enrich recieved update: ${util.inspect(update)}`);
+        debug(`enrich got context: ${util.inspect(oldContext)}`);
+        enrich(enrichers, oldContext, (err, context) => {
+            if (err) next(err);
+            else {
+                debug(`enrich sending new context: ${util.inspect(context)}`);
+                const oldContext = update[sessionPath[0]];
+                // update context in place by setting the first property on path to context
+                update[sessionPath[0]] = R.set(
+                    R.lensPath(sessionPath.splice(1)),
+                    context,
+                    oldContext);
+                next();
+            }
+        });
+    };
+
+module.exports = {EnrichIncomingWare};
